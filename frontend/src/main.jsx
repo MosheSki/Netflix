a0e513bcbde85c95aba14773b8e54c2bdb2b3000
MosheSki <MoshLago@gmail.com>
2024-03-13 09:12:46 +0200

    vercel deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,10 @@ import { AuthContextProvider } from "./authContext/AuthContext.jsx";
 
 axios.defaults.baseURL = "http://localhost:8080/api/";
 
+axios.defaults.baseURL = import.meta.env.DEV
+  ? "http://localhost:8080/api/"
+  : "https://netflix-beige-sigma.vercel.app";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
