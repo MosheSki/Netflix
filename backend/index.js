e24536a50d5b5f7af202d55e6238ea82c2c2b207
MosheSki <MoshLago@gmail.com>
2024-06-05 23:12:06 +0300

    added helmet (trying to fix the Dangerous site error)

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRouter.js";
 import contentRouter from "./routes/contentRouter.js";
 import listRouter from "./routes/listRouter.js";
+import helmet from "helmet";
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
 app.use("/api/users", userRouter);
 app.use("/api/contents", contentRouter);
